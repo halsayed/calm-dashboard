f3a5fb0f40e51020bca6f17c394bb0c5dcdb6b6a
wolfganghuse <wolfgang.huse@nutanix.com>
2021-09-16 19:59:44 +0200

    marketplace items

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import login_required, current_user
 from . import blueprint
-from .utils import list_deployed_apps
+from .utils import list_deployed_apps, list_marketplace_items
 
 
 @blueprint.route('/')
@@ -9,3 +9,13 @@ from .utils import list_deployed_apps
 def index():
     deployed_apps = list_deployed_apps(current_user)
     return render_template('dashboard/index.html', segment=['dashboard'], apps=deployed_apps)
+
+@blueprint.route('/taskselect/<task>', methods= ['GET', 'POST'])
+@login_required
+def taskselect(task):
+    if request.method == 'GET':
+        available_items = list_marketplace_items(current_user)
+        return str(available_items)
+        #return render_template('taskvm/index.html', segment=['virtual_machines'], marketplace_items=available_items)
+    if request.method == 'POST':
+        return '''abc'''
