214c93e1489b527adf5338e30417bf3aea73f1c4
Husain Ebrahim <husain@alsayed.ws>
2022-12-12 10:50:01 +0300

    divide by zero fix

--- app/dashboard/utils.py
+++ app/dashboard/utils.py
@@ -46,26 +46,39 @@ def project_meter(user: User):
         current_app.logger.debug('project_meter: Policy Engine active, no Quota set')
         meter['enabled']='false'
     else:
-        meter['enabled']='true'
-        meter['reserved_disk']=round(int(r.json()['entities'][0]['status']['resources']['reserved'].get('disk', 1))/1024/1024/1024)
+        meter['enabled'] = 'true'
+        meter['reserved_disk'] = round(
+            int(r.json()['entities'][0]['status']['resources']['reserved'].get('disk', 1))/1024/1024/1024)
+        meter['utilized_disk'] = round(
+            int(r.json()['entities'][0]['status']['resources']['utilized'].get('disk',0)) / 1024 / 1024 / 1024)
         current_app.logger.debug('project_meter: reserved_disk={}'.format(meter['reserved_disk']))
-        meter['utilized_disk']=round(int(r.json()['entities'][0]['status']['resources']['utilized']['disk'])/1024/1024/1024)
+        if meter['reserved_disk']:
+            meter['utilized_disk_percentage'] = round((meter['utilized_disk'] / meter['reserved_disk']) * 100)
+        else:
+            meter['utilized_disk_percentage'] = 0
         current_app.logger.debug('project_meter: utilized_disk={}'.format(meter['utilized_disk']))
-        meter['utilized_disk_percentage']=round((meter['utilized_disk']/meter['reserved_disk'])*100)
         current_app.logger.debug('project_meter: utilized_disk_percentage={}'.format(meter['utilized_disk_percentage']))
         
-        meter['reserved_memory']=round(int(r.json()['entities'][0]['status']['resources']['reserved']['memory'])/1024/1024/1024)
+        meter['reserved_memory'] = round(
+            int(r.json()['entities'][0]['status']['resources']['reserved'].get('memory', 0))/1024/1024/1024)
         current_app.logger.debug('project_meter: reserved_memory={}'.format(meter['reserved_memory']))
-        meter['utilized_memory']=round(int(r.json()['entities'][0]['status']['resources']['utilized']['memory'])/1024/1024/1024)
+        meter['utilized_memory'] = round(
+            int(r.json()['entities'][0]['status']['resources']['utilized'].get('memory', 0))/1024/1024/1024)
         current_app.logger.debug('project_meter: utilized_memory={}'.format(meter['utilized_memory']))
-        meter['utilized_memory_percentage']=round((meter['utilized_memory']/meter['reserved_memory'])*100)
+        if meter['reserved_memory']:
+            meter['utilized_memory_percentage'] = round((meter['utilized_memory']/meter['reserved_memory'])*100)
+        else:
+            meter['utilized_memory_percentage'] = 0
         current_app.logger.debug('project_meter: utilized_memory_percentage={}'.format(meter['utilized_memory_percentage']))
         
-        meter['reserved_vcpu']=int(r.json()['entities'][0]['status']['resources']['reserved']['vcpu'])
+        meter['reserved_vcpu'] = int(r.json()['entities'][0]['status']['resources']['reserved'].get('vcpu', 0))
         current_app.logger.debug('project_meter: reserved_vcpu={}'.format(meter['reserved_vcpu']))
-        meter['utilized_vcpu']=int(r.json()['entities'][0]['status']['resources']['utilized']['vcpu'])
+        meter['utilized_vcpu'] = int(r.json()['entities'][0]['status']['resources']['utilized'].get('vcpu', 0))
         current_app.logger.debug('project_meter: utilized_vcpu={}'.format(meter['utilized_vcpu']))
-        meter['utilized_vcpu_percentage']=round((meter['utilized_vcpu']/meter['reserved_vcpu'])*100)
+        if meter['reserved_vcpu']:
+            meter['utilized_vcpu_percentage'] = round((meter['utilized_vcpu']/meter['reserved_vcpu'])*100)
+        else:
+            meter['utilized_vcpu_percentage'] = 0
         current_app.logger.debug('project_meter: utilized_vcpu_percentage={}'.format(meter['utilized_vcpu_percentage']))
     return meter
  
